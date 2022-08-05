ff500f4accf9161dca04b478fc8d582c3d06f2f7
BalamuruganGnanakumar <balamurugan.gnanakumar@periscope-tech.com>
2022-08-05 13:07:31 +0530

    added new py files

---
+++ Hello_world.py
@@ -0,0 +1,3 @@
+import streanlit as st
+st.write("Hello Periscope Technologies")
+st.write("This is My first Streamlit app")
--- fees_calculation.py
+++ fees_calculation.py
@@ -1,7 +1,8 @@
+import streamlit as st
 a=10000
 r=0.07
 y=0
 while(a<20000):
     a=a+a*0.07
     y=y+1
-print("After",y,"Years the fees will be",a)
+st.write("After",y,"Years the fees will be",a)
